1d280c5045dc1a856395a7e015d7431c67602b9d
Alexander Paul Quinit <paulquinit@gmail.com>
2024-12-13 11:04:58 +0800

    Remove unused command options from Argo CLI

--- cmd/argo.go
+++ cmd/argo.go
@@ -24,31 +24,6 @@ func main() {
 		}
 		projectName := os.Args[2]
 		generator.CreateNewProject(projectName)
-	case "make:controller":
-		if len(os.Args) < 3 {
-			fmt.Println("Usage: argo make:controller <name>")
-			os.Exit(1)
-		}
-		name := os.Args[2]
-		generator.GenerateController(name)
-	// case "make:migration":
-	// 	if len(os.Args) < 3 {
-	// 		fmt.Println("Usage: argo make:migration <name>")
-	// 		os.Exit(1)
-	// 	}
-	// 	name := os.Args[2]
-	// 	generator.GenerateMigration(name)
-	// case "make:seeder":
-	// 	if len(os.Args) < 3 {
-	// 		fmt.Println("Usage: argo make:seeder <name>")
-	// 		os.Exit(1)
-	// 	}
-	// 	name := os.Args[2]
-	// 	generator.GenerateSeeder(name)
-	// case "migrate":
-	// 	migration.RunMigrations()
-	// case "seed":
-	// 	seeder.RunSeeders()
 	default:
 		fmt.Printf("Unknown command: %s\n", command)
 		os.Exit(1)
